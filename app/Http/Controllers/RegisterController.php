8602093a3c2dabd73795290bdc687388cde4c4c1
NedenIlya <nedenil27@gmail.com>
2024-12-14 19:45:43 +0300

    registration + jwt auth api

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request): JsonResponse
+    {
+        $request->validate([
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+
+        try {
+            $user = new User();
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            $response = [
+                'success' => true,
+                'message' => "Customer register successfully",
+                'user' => $user,
+            ];
+        } catch (QueryException $ex) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => $ex->getMessage()
+            ]);
+        }
+
+        Auth::setUser($user);
+
+        return response()->json($response);
+    }
+}
